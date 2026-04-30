3e8f3a69de8f9b5cde3b147a4dddc6de548fd909
enemy20135 <jules.bordier@outlook.com>
2026-04-30 20:04:36 +0200

    script qui marche peut etre

--- move.gd
+++ move.gd
@@ -1,7 +1,20 @@
 extends RigidBody3D
+@export var moveSpeed: float=5
 
 func _physics_process(delta):
-	pass
+	var left = "moveLeft"
+	var up = "moveLeft"
+	var down = "moveLeft"
+	var right = "moveLeft"
+	var vertical_axe= Input.get_axis(up,down)
+	var horizontal_axe= Input.get_axis(left,right)
+	var direction= Vector3(horizontal_axe,0,vertical_axe)
+	linear_velocity += direction * moveSpeed * delta
+	prints(direction)
+	var VitesseDeplassement = linear_velocity.length()
+#	if VitesseDeplassement > 5:
+#		look_at(global_position + linear_velocity)
+#		rotation.x = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
