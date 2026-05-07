828f320bd9fd53f11f83d9ab3b27c726549f4df7
enemy20135 <jules.bordier@outlook.com>
2026-05-07 20:32:58 +0200

    petit changement du script changement du modèle du perso pour que les bras soit séparé et pouvoir faire des animations et ajouts d'une premier animation

--- move.gd
+++ move.gd
@@ -3,9 +3,9 @@ extends RigidBody3D
 
 func _physics_process(delta):
 	var left = "moveLeft"
-	var up = "moveLeft"
-	var down = "moveLeft"
-	var right = "moveLeft"
+	var up = "moveUp"
+	var down = "moveDown"
+	var right = "moveRight"
 	var vertical_axe= Input.get_axis(up,down)
 	var horizontal_axe= Input.get_axis(left,right)
 	var direction= Vector3(horizontal_axe,0,vertical_axe)
